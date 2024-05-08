0e1574465c8acfd682b0d48529d801291f3ff7b9
IsraChido <2020030223@upsin.edu.mx>
2024-05-08 09:10:47 -0700

    pipesApp finalizado

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -19,4 +20,37 @@ export class UncommonPageComponent {
     this.name = 'Ashley';
     this.gender = 'female';
   }
+
+  // i18nPlural
+  public clients: string[] = ['Maria','Pedro','Fernando','Juan','Mateo','Ana','Kimberly','Ricardo']
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 personas esperando.',
+    'other': 'tenemos # clientes esperando.',
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  // KeyValue Pipe
+  public person = {
+    name: 'Israel',
+    age: 21,
+    address: 'Sinaloa, Mexico'
+  }
+
+  // Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap(value => console.log('tap:', value)),
+  );
+
+  public promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.');
+      console.log('Tenemos data en la promesa.');
+      // this.person.name = 'Otro nombre'
+    }, 3500);
+  })
 }
